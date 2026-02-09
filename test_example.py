2412a97587e3a1c1616f5c99df9a925ca39aa1bf
Ole Lensmar <ole@lensmar.com>
2026-02-09 15:44:28 +0100

    Update .gitignore to include .venv, *.xml, and __pycache__; adjust thresholds and actual output in test_example.py

--- test_example.py
+++ test_example.py
@@ -7,18 +7,18 @@ correctness_metric = GEval(
     name="Correctness",
     criteria="Determine if the 'actual output' is correct based on the 'expected output'.",
     evaluation_params=[LLMTestCaseParams.ACTUAL_OUTPUT, LLMTestCaseParams.EXPECTED_OUTPUT],
-    threshold=0.7,
+    threshold=0.8,
     verbose_mode=True
 )
 
 relevancy_metric = AnswerRelevancyMetric(
-    threshold=0.7,
+    threshold=0.8,
     include_reason=True
 )
 
 test_case = LLMTestCase(
     input="Is Testbusters Night in Vienna an amazing event?",
-    actual_output="Yes, TestBusters Night is a great event for testers and qa professionals with great talks and a lovely host.",
+    actual_output="Yes, TestBusters Night is a great event in Vienna for testers and qa professionals with great talks and a lovely host. And Vienna has great Schnitzel.",
     expected_output="Yes, TestBusters Night in Vienna is widely considered an excellent, high-energy event for software testing and QA professionals, with great talks and networking."
 )
 
